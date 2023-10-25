29d7c3e1a792a2e50ba2c7dfb9708c3fcd1d2e91
hemanth chowdary <yallankihemanth719@gmail.com>
2023-10-25 12:07:44 -0500

    updated mydata

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Yallanki Hemanth' });
+  res.render('mydata', { title: 'Yallanki Hemanth' });
 });
 
 module.exports = router;
